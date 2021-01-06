b57652c27f8742e8b6258f4930b9f185671e8c6d
szuzanna <zdrapka111@gmail.com>
2021-01-06 22:17:06 +0100

    visual changes

--- search_similar/urls.py
+++ search_similar/urls.py
@@ -3,5 +3,6 @@ from search_similar import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+	#path(r'^/(?P<warning>\w+)/$', views.main, name='main'),
     path('show/', views.show_results, name='show_results'),
 ]
--- search_similar/views.py
+++ search_similar/views.py
@@ -1,16 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from gensim.models import Word2Vec
+from django.urls import reverse
 
-def main(request):
-    return render(request, 'main.html',{})
+def main(request, **kwargs):
+    return render(request, 'main.html', kwargs)
 
 
 def show_results(request):
-    entities = request.GET.get('entities', 'ajajaj')
-    entities = entities.replace(" ", "")
+    entities = request.GET.get('entities', '')
+    entities = entities.replace(", ", ",")
     entities_list = entities.split(",")
 
     model = Word2Vec.load("search_similar/foodEmbedding.model")
-    similarities_list = model.most_similar(entities_list)
-
-    return render(request, 'show_results.html',{'entries': entities_list, 'similarities': similarities_list})
+    try:
+        similarities_list = model.most_similar(entities_list)
+    except:
+        #return render(None,'main.html', {'warning': "Make sure all node names are in database!"})
+        #return redirect(reverse('main', kwargs={'warning' :'Make sure all node names are in database!'}))
+        return redirect('main')#, kwargs={'warning':'Make sure all node names are in database!'})
+        
+    else:
+        return render(request, 'show_results.html',{'entries': entities_list, 'similarities': similarities_list})
